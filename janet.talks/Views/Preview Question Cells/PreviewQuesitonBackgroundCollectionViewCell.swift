1dfe214f973d5c2798e84b2adffc80933be9aa23
Kenton Cooley <kenton@darkwolf.ventures>
2021-11-08 18:50:17 -0600

    Questions ready to hook up to database

--- janet.talks/Views/Preview Question Cells/PreviewQuesitonBackgroundCollectionViewCell.swift
+++ janet.talks/Views/Preview Question Cells/PreviewQuesitonBackgroundCollectionViewCell.swift
@@ -12,32 +12,60 @@ class PreviewQuesitonBackgroundCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "PreviewQuesitonBackgroundCollectionViewCell"
     
+    private let backgroundBodyLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let bLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 27, weight: .heavy)
+        label.textColor = .secondaryLabel
+        label.text = "B:"
+        return label
+    }()
+    
     //MARK: - lifecycle
     
-    override init(frame: CGRect) {
+    override init(frame: CGRect){
         super.init(frame: frame)
-        
+        contentView.layer.cornerRadius = 25
+        contentView.layer.masksToBounds = true
+        contentView.addSubview(backgroundBodyLabel)
+        contentView.addSubview(bLabel)
+        clipsToBounds = true
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError()
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
+        bLabel.anchor(top: contentView.topAnchor, left: contentView.leftAnchor, paddingTop: 10, paddingLeft: spacing)
+              
+        backgroundBodyLabel.anchor(top: bLabel.bottomAnchor, left: contentView.leftAnchor, bottom: contentView.bottomAnchor, right: contentView.rightAnchor, paddingTop: 5, paddingLeft: spacing, paddingBottom: 10, paddingRight: spacing)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        
+        backgroundBodyLabel.text = nil
     }
     
     //MARK: - actions
     
+    @objc private func readMoreButtonTapped(){
+        
+    }
+    
     //MARK: - helpers
     
-    public func configure(with viewModel: PreviewQuestionBackgroundViewModel) {
+    func configure(with viewModel: PreviewQuestionBackgroundViewModel){
         
+        contentView.backgroundColor = .systemBackground
+        backgroundBodyLabel.text = viewModel.background
     }
 }
